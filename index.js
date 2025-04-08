6b0fe8351f7363e670df0dd2b2b5a564ce1b1dbf
DESKTOP-QRG9KB3\gennait <gennait@e-garde.co.jp>
2025-04-08 14:19:20 +0900

    最初のコミット

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+const activities = require("./activities.json");
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/autumn",function(req,res){
+    fs.writeFile(__dirname + "/data.txt",
+        req.body.activity,
+        function() {
+            res.send("投稿完了");
+        });
+});
+
+app.post("/update", function (req, res) { // 修正
+    activities[0].activity = req.body.updatedActivity;
+    res.send(activities);
+});
+
+app.post("/delete", function (req, res) {
+    activities.splice(req.body.number, 1);
+    res.send(activities);
+});
+
+const port = process.env.PORT || 5000;    // 追加
+
+app.listen(port, function() {       // 修正
+    console.log(`Listening on ${port}`);   // 修正
+});
